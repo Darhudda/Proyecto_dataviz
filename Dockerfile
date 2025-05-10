df5f4c9d9733b5d3650f6d814a95abc6ef5e5e5e
Darhudda <erodriguezp@uninorte.edu.co>
2025-05-10 07:57:55 -0500

    Versión inicial del proyecto con Docker y conexión a PostgreSQL

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Imagen base de Python
+FROM python:3.10-slim
+
+# Establecer directorio de trabajo
+WORKDIR /app
+
+# Copiar archivos del proyecto
+COPY requirements.txt .
+COPY app_project.py .
+COPY cargar_datos.py .
+COPY dataset_final.csv .
+
+
+# Instalar dependencias
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Exponer el puerto de Dash
+EXPOSE 8050
+
+# Comando para correr la app
+CMD ["python", "app_project.py"]
---
+++ cargar_datos.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Cargar archivo CSV
+df = pd.read_csv("dataset_final.csv", encoding='latin-1')
+print("Primeros registros del archivo:")
+print(df.head())
+
+# Conexión a PostgreSQL
+#usuario = "postgres"
+#contraseña = "eliana20062004"  
+#host = "localhost"
+#puerto = "5432"
+#base_datos = "dataviz_db"
+
+# Crear conexión
+engine = create_engine("postgresql+psycopg2://postgres:eliana20062004@db:5432/dataviz_db")
+
+# Insertar los datos
+df.to_sql("fraude", engine, if_exists="replace", index=False)
+
+print("Datos cargados correctamente.")
+
